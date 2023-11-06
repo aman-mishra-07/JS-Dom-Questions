82c315f4c12b307d5c69f81a4cf41c968cfd713d
aman-mishra-07 <AMAN7986355740@GMAIL.COM>
2023-11-06 11:59:53 +0530

    Promises and setTimeout

---
+++ src/advanceJs/promises.js
@@ -0,0 +1,39 @@
+//Q3 
+
+// function calljob() {
+//     let myPromise = new Promise((resolve) =>{
+//         setTimeout(function(){
+//             resolve('Hello world')
+//         },2000)
+//     })
+//     return myPromise.then((value) => {
+//         console.log(value)
+//     })
+    
+// }
+
+// calljob();
+
+function checkOddEven(num){
+    let checkNumber = new Promise((resolve, reject) =>{
+        if(num % 2 === 0){
+            resolve('even')
+        }
+        if(num % 1 === 0){
+            resolve('odd')
+        }
+        if(isNaN(num)){
+            reject('Not a Number')
+        }
+    })
+    checkNumber.then((res) => {
+        console.log(res);
+    })
+    checkNumber.catch((rej) => {
+        console.log(rej);
+    })
+}
+
+checkOddEven(5)
+checkOddEven(2)
+checkOddEven('uhkdj')
